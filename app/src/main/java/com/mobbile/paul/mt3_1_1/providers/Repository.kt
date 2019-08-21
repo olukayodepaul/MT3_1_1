81e89e829cc8979907a6b3879bc52b657407201b
olukayodepaul <paul.olukayode.pro@gmail.com>
2019-08-21 13:17:11 +0100

    Initial commit

--- app/src/main/java/com/mobbile/paul/mt3_1_1/providers/Repository.kt
+++ app/src/main/java/com/mobbile/paul/mt3_1_1/providers/Repository.kt
@@ -15,36 +15,52 @@ class Repository @Inject
 constructor(private val appDao: AppDao, private val api: Api, private var mapi: MapApi) {
 
     fun getUsers(username: String, password: String, imei: String): Single<Response<EmployeesApi>> =
-           api.getUser(username, password, imei)
+        api.getUser(username, password, imei)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .map {it}
+            .map { it }
 
-    fun createModules(employee: List<ModulesRoom>,
-                      customers: List<Bank_n_CustomersRoom>,
-                      products: List<ProductsRoom>,
-                      producttype: List<ProductTypeRoom>) =
-        Observable.fromCallable { appDao.saveEmployees(employee,customers,products,producttype)}
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
+    fun createModules(
+        employee: List<ModulesRoom>,
+        customers: List<Bank_n_CustomersRoom>,
+        products: List<ProductsRoom>,
+        producttype: List<ProductTypeRoom>
+    ) =
+        Observable.fromCallable { appDao.saveEmployees(employee, customers, products, producttype) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
 
     fun fetchModules(): Single<List<ModulesRoom>> =
-        Single.fromCallable {appDao.fetchModules()}
+        Single.fromCallable {
+            appDao.fetchModules()
+        }
 
     fun fetchCustomers(): Observable<List<Bank_n_CustomersRoom>> =
-        Observable.fromCallable {appDao.fetchCustomers()}
+        Observable.fromCallable {
+            appDao.fetchCustomers()
+        }
 
-    fun fetchBasket(sep:Int): Observable<List<ProductsRoom>> =
-        Observable.fromCallable {appDao.fetchBasket(sep)}
+    fun fetchBasket(sep: Int): Observable<List<ProductsRoom>> =
+        Observable.fromCallable {
+            appDao.fetchBasket(sep)
+        }
 
-    fun fetchGoogleMap(origin: String, destination: String, sensor: String, mode: String, key: String): Single<GoogleGetApi> =
+    fun fetchGoogleMap(
+        origin: String,
+        destination: String,
+        sensor: String,
+        mode: String,
+        key: String
+    ): Single<GoogleGetApi> =
         mapi.getGooleMap(origin, destination, sensor, mode, key)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .map {it}
 
     fun fetchDailySales(): Observable<List<SalesEntriesRoom>> =
-        Observable.fromCallable{appDao.fetchDailySales()}
+        Observable.fromCallable {
+            appDao.fetchDailySales()
+        }
 
     companion object {
         private val TAG = "Repository"
--- app/src/main/java/com/mobbile/paul/mt3_1_1/ui/sales/sales/salesentries/SalesEntriesViewModel.kt
+++ app/src/main/java/com/mobbile/paul/mt3_1_1/ui/sales/sales/salesentries/SalesEntriesViewModel.kt
@@ -1,10 +1,36 @@
 package com.mobbile.paul.mt3_1_1.ui.sales.sales.salesentries
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.kotlin_project.providers.Repository
+import com.mobbile.paul.mt3_1_1.models.SalesEntriesRoom
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class SalesEntriesViewModel @Inject constructor(val repository: Repository) : ViewModel() {
 
+      fun fetchDailySales(): LiveData<List<SalesEntriesRoom>> {
+
+          var mResult = MutableLiveData<List<SalesEntriesRoom>>()
+          repository.fetchDailySales()
+              .subscribeOn(Schedulers.io())
+              .observeOn(AndroidSchedulers.mainThread())
+              .subscribe({
+                  mResult.postValue(it)
+                  Log.d(TAG,"onObserve "+ it as ArrayList<SalesEntriesRoom>)
+              },{
+                  mResult.postValue(null)
+              }).isDisposed
+          return mResult
+      }
+
+    companion object {
+        var TAG = "SalesEntriesViewModel"
+    }
 
 }
+
+
